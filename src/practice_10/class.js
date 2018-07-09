a5ef0a4962babad33c6e592e670f075b02780350
jessie <jessie.xu@oocl.com>
2018-07-10 00:53:52 +0800

    [jessie] complete the practice 10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,27 @@
 
+class Class{
+    constructor(number){
+        this.number = number;
+    }
+    leader;
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student){
+        if(student.klass.number === this.number)
+            this.leader = student;
+        else {
+                this.leader = undefined;
+                console.log("It is not one of us.");
+        }
+    }
+    appendMember(student) {
+        student.klass = this;
+    }
+}
+
+
+module.exports={
+    Class
+}
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,26 @@
+const { Person } = require('./person')
+const { Class } = require('./class')
 
+class Teacher extends Person {
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+    introduce() {
+        if (this.klasses === undefined || this.klasses.length === 0)
+            return `${super.introduce()} I am a Teacher. I teach No Class.`;
+        else
+            return `${super.introduce()} I am a Teacher. I teach Class ${this.klasses.map(x => x.number).join(", ")}.`;
+    }
+    introduceWith(student) {
+        let isStudent = this.klasses.some(klass => klass === student.klass);
+        if (isStudent) {
+            return `${super.introduce()} I am a Teacher. I teach ${student.name}.`;
+        } else {
+            return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`;
+        }
+    }
+}
+module.exports = {
+    Teacher
+}
