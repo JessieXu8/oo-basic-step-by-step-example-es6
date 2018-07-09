6343a33b01209e401301bbe01afa3ff02d0297d7
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-09 16:42:02 +0800

    [jessie] completed the practice of 1-7

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,16 @@
 
+const {Person} = require('./person')
+const {Class} = require('./class')
+
+class Student extends Person{
+    constructor(name,age,klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+    introduce(){
+        return super.introduce()+' I am a Student. I am at Class '+this.klass.number+'.';
+    }
+}
+module.exports = {
+    Student
+}
